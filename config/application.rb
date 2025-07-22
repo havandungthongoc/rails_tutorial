62ca212fad5fdbbb1635cbee0151022a2dbd3e3e
dunhhadka <108753157+dunhhadka@users.noreply.github.com>
2025-07-22 09:54:25 +0700

    Chapter 3, 4, 5

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,10 @@ module RailsTutorial
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Thiết lập I18n
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    get "/home",    to: "static_pages#home", as: :home
+    get "/help",    to: "static_pages#help", as: :help
+    get "/contact", to: "static_pages#contact", as: :contact
+
+    # resources :microposts (removed because no controller exists)
+  end
 end
