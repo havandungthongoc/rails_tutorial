19bf789049133e870f1a5ef31e10801f7c542561
dunhhadka <108753157+dunhhadka@users.noreply.github.com>
2025-07-22 09:54:25 +0700

    Chapter 3, 4, 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/home",    to: "static_pages#home"
+  get "/help",    to: "static_pages#help"
+  get "/contact", to: "static_pages#contact"
+
+  resources :microposts
 end
